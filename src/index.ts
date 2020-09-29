7dfc65985bfa3a54ce666a48b5f02564a7e0a695
sujiaheng <648952316@qq.com>
2020-09-29 16:48:16 +0800

    chore(iframewrapper.tsx): rename file

--- src/index.ts
+++ src/index.ts
@@ -238,6 +238,7 @@ export class IframeBridge extends InvisiblePlugin<IframeBridgeAttributes> {
             }
             default: {
                 console.warn(`${data.kind} not allow event.`);
+                break;
             }
         }
     }
